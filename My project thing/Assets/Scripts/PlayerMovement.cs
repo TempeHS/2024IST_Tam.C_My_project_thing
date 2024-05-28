b28210c3b91b72cb40cb66532722080871efa069
timthetamman <127055360+timthetamman@users.noreply.github.com>
2024-05-28 11:55:04 +1000

    change gravity scale

--- My project thing/Assets/Scripts/PlayerMovement.cs
+++ My project thing/Assets/Scripts/PlayerMovement.cs
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0.5f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
