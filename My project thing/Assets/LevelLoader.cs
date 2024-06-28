8b55dcbebb1710ca0e6acb2de5c347659cb20fbd
timthetamman <127055360+timthetamman@users.noreply.github.com>
2024-06-28 11:42:16 +1000

    added animation fade

--- My project thing/Assets/LevelLoader.cs
+++ My project thing/Assets/LevelLoader.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    
+    public Animator transition;
+
+    public float transitionTime = 1f;
 
     // Update is called once per frame
     void Update()
@@ -17,16 +20,17 @@ public class LevelLoader : MonoBehaviour
 
     public void LoadNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        
     }
 
     IEnumerator LoadLevel(int levelIndex)
     {
-        //Play animation
+       transition.SetTrigger("Start");
 
-        //Wait
+       yield return new WaitForSeconds(1);
 
-        //Load scene
+        SceneManager.LoadScene(levelIndex);
     }
 
 
